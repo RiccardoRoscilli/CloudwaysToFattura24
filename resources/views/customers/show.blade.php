c257c3a1772782ac2f5cdd1cb33cf8d4b16aeaac
Riccardo <riccardo@intraweb.it>
2026-04-02 10:05:48 +0200

    rimosso form associa application dalla show cliente

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -26,20 +26,6 @@
             @endif
         </div>
 
-        <!-- Form per associare una nuova Application -->
-        <form action="{{ route('customers.addApplication', $customer->id) }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="application">Seleziona Application:</label>
-                <select name="application_id" id="application_id" class="form-control">
-                    @foreach($applications as $application)
-                        <option value="{{ $application->id }}">{{ $application->label }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary mt-2">Associa Application</button>
-        </form>
-
         <div class="mt-4">
             <h2>Servizi (Domini/Hosting)</h2>
             @if($services->count() > 0)
